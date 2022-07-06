c1335fb0013e26d213c57f7e986a2b9c3efd3f64
TallRedHead <102986777+TallRedHead@users.noreply.github.com>
2022-07-06 13:36:39 -0500

    Update auto_gitpull.sh

--- auto_gitpull.sh
+++ auto_gitpull.sh
@@ -5,6 +5,6 @@ now=$(date +"%T")
 echo "Attempting to git pull at : $now"
 cd Brake_Press/
 sudo git pull --rebase
-sudo chmod +x PressDisplay2.py
+sudo chmod +x PressDisplay3.py
 sudo chmod +x auto_gitpull.sh
 
