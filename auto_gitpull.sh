4f2f7389f2126dddea80160e04a8625967e44152
TallRedHead <102986777+TallRedHead@users.noreply.github.com>
2022-07-13 14:20:27 -0500

    added git stash

--- auto_gitpull.sh
+++ auto_gitpull.sh
@@ -4,6 +4,8 @@ sleep 10
 now=$(date +"%T")
 echo "Attempting to git pull at : $now"
 cd Brake_Press/
+sudo git stash
+sleep 1
 sudo git pull --rebase
 sudo chmod +x PressDisplay3.py
 sudo chmod +x auto_gitpull.sh
